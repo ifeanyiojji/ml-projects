88e5b9dcafee850537a06df433172d8d7d7d3241
ifeanyi_ojji <shedrachifeanyi725@gmail.com>
2025-01-12 21:06:17 +0100

    updated the predict_pipeline file

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -14,7 +14,7 @@ class PredictPipeline:
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
-            return preds
+            return preds 
         except Exception as e:
             raise CustomException(e,sys)
     
