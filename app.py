35aaaa8dec6088ab454723ceaf82666bee67a019
ifeanyi_ojji <shedrachifeanyi725@gmail.com>
2025-01-13 21:12:53 +0100

    corrected an error in the app.py, updated the home.html file and also corrected the directory of the of the preprocessor.pkl

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict_datapoint():
     else:
         data=CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethinicity'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
@@ -37,4 +37,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
